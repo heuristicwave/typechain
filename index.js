9a95f1545e05cf103e696fb407c628a6e9e0eabd
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2019-12-28 16:59:20 +0900

    create learning typescript

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "HeuristicWave", age = 7, gender = "male";
+// ? param option
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+sayHi(name, age, gender);
+//# sourceMappingURL=index.js.map
---
+++ notes.md
@@ -0,0 +1,16 @@
+
+
+```
+const name = "HeuristicWave",
+  age = 7,
+  gender = "male";
+
+// ? param option
+const sayHi = (name, age, gender?) => {
+  console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+
+sayHi(name, age, gender);
+
+export {};
+```
