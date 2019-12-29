97a45cbc78d0882e2aef23297293b2ffaaa70ef0
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2019-12-29 12:45:45 +0900

    Create typeTutorial.ts

---
+++ src/typeTutorial.ts
@@ -0,0 +1,20 @@
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
+    constructor(name: string, age: number, gender: string) {
+      this.name = name;
+      this.age = age;
+      this.gender = gender;
+    }
+  }
+  
+  const logi = new Human("Logi", 18, "female");
+  
+  const sayHi = (person: Human): string => {
+      return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
+  };
+    
+  console.log(sayHi(logi));
+    
+  export {};
