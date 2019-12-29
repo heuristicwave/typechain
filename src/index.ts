c65a950be8aa45d1dd515a13866dc5a0e53ff653
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2019-12-30 00:53:00 +0900

    add basic block structure

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
+import * as CryptoJS from "crypto-js";
+
 class Block {
   public index: number;
   public hash: string;
   public previousHash: string;
   public data: string;
   public timestamp: number;
+
+  static calculateBlockHash =(
+    index:number,
+    previousHash:string,
+    timestamp:number,
+    data:string
+    ) :string => 
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
   constructor(
     index: number,
     hash: string,
@@ -19,9 +30,17 @@ class Block {
   }
 }
 
+// Block.calculateBlockHash();  // be able to func, the declaration as static
+
 const genesisBlock: Block = new Block(0, "2020202020202", "", "Hello", 123456);
 
-let blockchain: [Block] = [genesisBlock];
-console.log(blockchain);
+let blockchain: Block[] = [genesisBlock];
+
+const getBlockchain = () : Block[] => blockchain;
+
+const getLastBlock = () : Block => blockchain[blockchain.length -1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+//console.log(blockchain);
 
 export {};
